2a3273c3d7e5ea7c8a9e281941ab8a6dc7820d9f
erdem <altinsoyyusuferdem@gmail.com>
2026-04-17 19:54:46 +0300

    chore: Temel klasör yapısı ve scriptler

---
+++ src/data_loading/read.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parents[2]
+
+# m: kaç metreyi bir nokta olarak kabul etmek istediğimiz.
+# Minimum 1 olabilir burada 1320 nin tam böleni olmak zorundaki verilerde kayma yada boş satır sorunu görmeyelim
+
+def read(m):
+
+    csv_path = ROOT / "data" / "raw" / "sem_2025_eu.csv"
+    df = pd.read_csv(csv_path) #n
+
+    utmx = df["UTMX"].to_numpy()
+    utmy = df["UTMY"].to_numpy()
+
+    segment_count = len(utmx) - 1
+
+    if m <= 0:
+        raise ValueError("m pozitif bir tam sayı olmalı")
+    
+    if segment_count % m != 0:
+        raise ValueError(f"Segment length {segment_count} değeri m={m} ile tam bölünmüyor!")
+
+
+    elevation = df["Elevation (m)"].to_numpy()
+    distance = df["Distance from Lap Line (m)"].to_numpy()
+
+    dx = np.diff(utmx)
+    dy = np.diff(utmy)
+    length = distance[-1]
+
+    seq_length = np.hypot(dx, dy) #n-1
+    seq_heading = np.unwrap(np.arctan2(dy, dx)) #n-1
+    seq_heading_change = np.diff(seq_heading) #n-2
+    seq_track_progress = np.array(distance[1::] / length) #n-1
+    seq_slope = np.array(np.diff(elevation) / seq_length) #n-1
+    seq_curvature = np.array(seq_heading_change / np.convolve(seq_length, [0.5, 0.5], mode="valid")) #n-2   
+
+    
+    return {
+        'seq_x':utmx[1:].reshape(-1, m)[:, -1],
+        'seq_y':utmy[1:].reshape(-1, m)[:, -1],
+        'seg_length':seq_length.reshape(-1, m).sum(axis=1),
+        'seg_heading':seq_heading.reshape(-1, m).mean(axis=1),
+        'seg_heading_change': np.insert(seq_heading_change, 0, np.nan).reshape(-1, m).mean(axis=1),
+        'seg_track_progress': seq_track_progress.reshape(-1, m)[:, -1],
+        'seg_slope': seq_slope.reshape(-1, m).mean(axis=1),
+        'seg_curvature': np.insert(seq_curvature, 0, np.nan).reshape(-1, m).mean(axis=1)
+        #Burda n-2 değerlerinin başına sorun çıkasın diye nan ekliyorum fakat bize sorun çıkarabilecek bir yöntem düzeltirmesi gerek
+    }
+    
+
+def create_feature_table():
+    data = read(8)
+
+    output_path = ROOT / "data" / "processed" / "track_features.csv"
+
+    df = pd.DataFrame(
+        np.column_stack(list(data.values())),
+        columns = data.keys()
+    )
+    
+    df.to_csv(output_path, index=False)
+
+
+if __name__ == "__main__":
+    create_feature_table()
---
+++ src/visualization/draw.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from pathlib import Path
+
+def load_data():
+    root = Path(__file__).resolve().parents[2]
+    raw_path = root / "data" / "raw" / "sem_2025_eu.csv"
+    features_path = root / "data" / "processed" / "track_features.csv"
+
+    raw_df = pd.read_csv(raw_path)
+    features_df = pd.read_csv(features_path)
+    return raw_df, features_df
+
+
+def draw_2d():
+    df, tf = load_data()
+
+    fig, ax = plt.subplots(3, 1, figsize=(12, 14))
+
+    ax[0].plot(df["UTMX"], df["UTMY"])
+    ax[0].set_aspect('equal')
+    ax[0].set_title("Pist")
+
+    sc1 = ax[1].scatter(tf["seq_x"], tf["seq_y"], c=abs(tf["seg_curvature"]), cmap="coolwarm", s=3)
+    ax[1].set_aspect('equal')
+    ax[1].set_title("Curvature")
+    
+    sc2 = ax[2].scatter(tf["seq_x"], tf["seq_y"], c=tf["seg_slope"], cmap="coolwarm", s=3)
+    ax[2].set_aspect("equal")
+    ax[2].set_title("Slope")
+    
+    plt.colorbar(sc2, ax=ax[2], label="Slope")
+    plt.colorbar(sc1, ax=ax[1], label="Curvature")
+    plt.show()
+
+def draw_3d():
+    
+    df, tf = load_data()
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+
+    z = df["Elevation (m)"]
+    ax.plot(df["UTMX"], df["UTMY"], (z - z.min()))
+    ax.set_zlim(0, 12)
+    plt.show()
+
+if __name__ == "__main__":
+    draw_2d()
